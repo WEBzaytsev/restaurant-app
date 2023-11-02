2500329ebd8d44e1d81f6b0ca744cc2052850991
WEBzaytsev <nikita@zaitsv.dev>
2023-11-02 23:01:39 +0200

    set home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,32 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div class="container">
+      <HomeHeader />
+      <ProductsList />
+    </div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
+import ProductsList from "@/components/ProductList.vue";
+import HomeHeader from "@/components/HomeHeader.vue";
 
 export default {
   name: "HomeView",
-  components: {},
+  components: { HomeHeader, ProductsList },
 };
 </script>
+
+<style>
+.home {
+  padding: 54px 20px 100px;
+  background-color: var(--black-color);
+  color: white;
+  min-height: 100vh;
+}
+
+.container {
+  max-width: 1300px;
+  margin: 0 auto;
+}
+</style>
