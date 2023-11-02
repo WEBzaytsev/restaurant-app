7561fb73e9f2ccd90c90301a22d990f1fc9dc1bc
WEBzaytsev <nikita@zaitsv.dev>
2023-11-02 22:59:30 +0200

    create a Header component

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,37 @@
+<script>
+import ColorButton from "@/components/ColorButton.vue";
+
+export default {
+  name: "HomeHeader",
+  components: { ColorButton },
+};
+</script>
+
+<template>
+  <header class="header">
+    <h1 class="caption">наша продукция</h1>
+    <div class="mini-cart">
+      <router-link to="/login">
+        <ColorButton text="Войти" color="orange" />
+      </router-link>
+
+      <ColorButton text="Выйти" color="black" />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+  margin-bottom: 75px;
+}
+
+.caption {
+  font-size: 31px;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+</style>
