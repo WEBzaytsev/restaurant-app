a995ec0c667bd2ce2dc7f9a4aa134b77007a1199
WEBzaytsev <nikita@zaitsv.dev>
2023-11-02 18:45:02 +0200

    create auth view

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,18 @@
 </template>
 
 <style>
+:root {
+  --black-color: #161516;
+  --orange-color: #d58c51;
+  --red-color: #ff0b0b;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  color: var(--black-color);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/views/AuthView.vue
@@ -0,0 +1,285 @@
+<template>
+  <div class="auth">
+    <div class="overlay"></div>
+    <div class="auth-modal">
+      <router-link class="auth-modal-change_auth" :to="changeAuthLink">
+        {{ changeAuthText }}
+      </router-link>
+
+      <p class="auth-title">{{ title }}</p>
+      <form class="auth-form" @submit.prevent="submitHandler">
+        <div class="auth-form-field">
+          <input
+            type="text"
+            class="auth-form-input"
+            :class="{
+              invalid: errors.login,
+            }"
+            v-model="login"
+            placeholder="Логин"
+            @input="resetLoginError"
+          />
+          <span class="auth-form-error" v-if="errors.login">{{
+            errors.login
+          }}</span>
+        </div>
+        <div class="auth-form-field">
+          <input
+            type="password"
+            class="auth-form-input"
+            :class="{
+              invalid: errors.password,
+            }"
+            v-model="password"
+            placeholder="Пароль"
+            @input="resetPasswordError"
+          />
+          <span class="auth-form-error" v-if="errors.password">{{
+            errors.password
+          }}</span>
+        </div>
+        <label for="is-agree" class="checkbox-label" v-if="!isLogin">
+          <input type="checkbox" :checked="isAgree" id="is-agree" hidden />
+          <span>Я согласен получать обновления на почту</span>
+        </label>
+        <div class="auth-button-wrap">
+          <button class="auth-button" type="submit">{{ buttonText }}</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  login: yup
+    .string()
+    .required("Введите логин")
+    .min(5, "Логин должен содержать не менее 5 символов")
+    .max(25, "Логин должен содержать не более 25 символов"),
+  password: yup
+    .string()
+    .required("Введите пароль")
+    .min(8, "Пароль должен содержать не менее 8 символов")
+    .max(25, "Пароль должен содержать не более - 25 символов"),
+});
+
+export default {
+  name: "AuthView",
+  data() {
+    return {
+      login: "",
+      password: "",
+      isAgree: false,
+      errors: {
+        login: null,
+        password: null,
+      },
+    };
+  },
+  computed: {
+    isLogin() {
+      return this.$route.name === "login";
+    },
+    changeAuthLink() {
+      return this.isLogin ? "/register" : "/login";
+    },
+    changeAuthText() {
+      return this.isLogin ? "Зарегистрироваться" : "Авторизоваться";
+    },
+    title() {
+      return this.isLogin ? "Вход" : "Регистрация";
+    },
+    buttonText() {
+      return this.isLogin ? "Войти" : "Зарегистрироваться";
+    },
+  },
+  methods: {
+    async submitHandler() {
+      try {
+        await schema.validate(
+          {
+            login: this.login,
+            password: this.password,
+          },
+          { abortEarly: false }
+        );
+      } catch (e) {
+        const errors = e.inner;
+
+        if (!errors.length) {
+          this.errors = {};
+        } else {
+          this.errors.login = errors.find(
+            (error) => error?.path === "login"
+          )?.errors[0];
+          this.errors.password = errors.find(
+            (error) => error?.path === "password"
+          )?.errors[0];
+        }
+      }
+    },
+
+    resetLoginError() {
+      this.errors.login = null;
+    },
+
+    resetPasswordError() {
+      this.errors.password = null;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.auth {
+  background-image: url("@/assets/auth-bg.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.overlay {
+  position: absolute;
+  inset: 0;
+  z-index: 1;
+  background-color: rgba(0, 0, 0, 0.45);
+}
+
+.auth-modal {
+  position: relative;
+  z-index: 2;
+  background-color: #fff;
+  padding: 36px 20px 34px;
+  border-radius: 6px;
+  max-width: 460px;
+  width: 100%;
+}
+
+.auth-title {
+  margin-bottom: 36px;
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 31px;
+  font-weight: bold;
+}
+
+.auth-modal-change_auth {
+  position: absolute;
+  display: block;
+  top: 7px;
+  left: auto;
+  right: 20px;
+  text-decoration: underline;
+  font-size: 12px;
+  font-weight: 300;
+  color: var(--orange-color);
+}
+
+.auth-form {
+  display: grid;
+  row-gap: 15px;
+}
+
+.auth-form-input.invalid {
+  border-color: var(--red-color);
+}
+
+.auth-form-input {
+  display: block;
+  background-color: #fff;
+  border: 1px solid var(--orange-color);
+  border-radius: 60px;
+  padding: 9px 18px 10px;
+  width: 100%;
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+.checkbox-label {
+  display: block;
+  width: 100%;
+  cursor: pointer;
+  box-sizing: border-box;
+}
+
+.checkbox-label span {
+  position: relative;
+  display: block;
+  padding: 2px 0 2px 27px;
+  text-align: left;
+  user-select: none;
+}
+
+.checkbox-label span:before {
+  content: " ";
+  box-sizing: border-box;
+  display: block;
+  width: 18px;
+  height: 18px;
+  position: absolute;
+  left: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  border-radius: 50%;
+  border: 1px solid var(--orange-color);
+}
+
+.checkbox-label span:after {
+  content: none;
+  box-sizing: border-box;
+  display: block;
+  width: 10px;
+  height: 10px;
+  position: absolute;
+  left: 4px;
+  top: 50%;
+  transform: translateY(-50%);
+  border-radius: 50%;
+  background-color: var(--orange-color);
+}
+
+.checkbox-label input:checked + span:after {
+  content: " ";
+}
+
+.auth-button-wrap {
+  padding-top: 13px;
+}
+
+.auth-button {
+  display: block;
+  width: 200px;
+  margin: 0 auto;
+  cursor: pointer;
+  background-color: var(--orange-color);
+  border-radius: 4px;
+  text-align: center;
+  padding: 12px 0;
+  font-size: 14px;
+  color: var(--black-color);
+  border: none;
+  outline: none;
+}
+
+.auth-form-error {
+  color: var(--red-color);
+  text-align: left;
+  position: absolute;
+  left: 18px;
+  top: 102%;
+  font-weight: 300;
+  font-size: 12px;
+}
+
+.auth-form-field {
+  position: relative;
+}
+</style>
