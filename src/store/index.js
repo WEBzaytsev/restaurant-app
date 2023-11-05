891a631102e53d058719df2749345d897493af2e
WEBzaytsev <nikita@zaitsv.dev>
2023-11-05 22:35:13 +0200

    fix reduce total calculated

--- src/store/index.js
+++ src/store/index.js
@@ -100,7 +100,7 @@ export default createStore({
           (cartItem) => item.id === cartItem.id
         );
 
-        if (!productInCart) return 0;
+        if (!productInCart) return total + 0;
 
         return total + productInCart.count * item.price;
       }, 0);
